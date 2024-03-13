26734a8b9223619d854e178a7255ab6b85e4008f
Kusuma-321 <siripurapukusumakumari@gmail.com>
2024-03-13 21:56:43 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useHistory, Switch } from 'react-router-dom';
+import Header from "./Header";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
+import { Data } from "./Data";
+import { useState } from "react";
+import Card from "./Card";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+
 
 function App() {
+  const history = useHistory();
+
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: "",
+      onCommand: ({ command, product }) => {
+        if (command === "show") {
+          filter(product);
+        }
+        if (command === "cartPage") {
+          history.push("/cart");
+        }
+        if (command === "ordersPage") {
+          history.push("/orders");
+        }
+        if (command === "homePage") {
+          history.push("/");
+        }
+      },
+    });
+    filter("");
+  }, [history]);
+
+  const filter = (names) => {
+    const filtered = Data.filter((item) => item.name.includes(names));
+    console.log(filtered);
+    setCategory(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <div style={{ display: "flex", flexWrap: "wrap" }}>
+            {category.map((item) => (
+              <Card
+                image={item.image}
+                name={item.name}
+                rating={item.rating}
+                actualPrice={item.actualPrice}
+                offerPrice={item.offerPrice}
+              />
+            ))}
+          </div>
+        </Route>
+
+        <Route exact path="/cart">
+          <Navbar text={"This is Cart Page"} />
+        </Route>
+        <Route exact path="/orders">
+          <Navbar text={"This is Orders Page"} />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
